33c6bb89b732266993d56159a53ac1247781fa16
Sharmr <s98raghav@gmail.com>
2021-12-07 13:11:20 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# An online chess game built with react that you can play with other players online
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Board from './components/Board.js';
+
+export default App = () => {
+    return (<div>
+                Something
+            </div>)
+};
